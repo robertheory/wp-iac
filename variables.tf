1a8676363e5351ce98e4606c877520ef52f59716
Roberto Costa <roberto.araujo339@gmail.com>
2024-07-10 17:12:39 -0300

    terraform and ansible configs created

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "vm_user" {
+  default = "ubuntu"
+}
+
+variable "name_security_group" {
+  default = "wp-security-group"
+}
+
+variable "ami_aws_instance" {
+  default = "ami-09040d770ffe2224f"
+}
+
+variable "type_aws_instance" {
+  default = "t2.micro"
+}
+
+variable "key_aws_instance" {
+  default = "wp-key"
+}
+
+variable "provisioner_remote_exec_private_key" {
+  default = "./wp-key.pem"
+}
